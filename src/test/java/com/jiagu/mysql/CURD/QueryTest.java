f225758e171d5c20e6daa33b1c488c4e5b99756b
park.yq <park.yq@alibaba-inc.com>
2019-02-20 13:46:42 +0800

    remove secert information

--- src/test/java/com/jiagu/mysql/CURD/QueryTest.java
+++ src/test/java/com/jiagu/mysql/CURD/QueryTest.java
@@ -7,19 +7,12 @@ public class QueryTest {
 
     public static void main(String args[]) throws Exception{
         Query query = new Query();
-//        String host = "192.168.43.97";
-//        int port = 3306;
-//        String user = "root";
-//        String password = "tdlab401";
-//        String dataBase = "data";
-//        String sqlStr = "SELECT name,author FROM `paper` limit 0,2;";
-
-        String sqlStr = "select * from test limit 1";
-        String host = "10.101.164.148";
-        int port = 30001;
-        String user = "nanxian";
-        String password = "nanxian_test";
-        String dataBase = "test4dmp";
+        String host = "192.168.43.97";
+        int port = 3306;
+        String user = "root";
+        String password = "tdlab401";
+        String dataBase = "data";
+        String sqlStr = "SELECT name,author FROM `paper` limit 0,2;";
 
 
         query.query(host,port,user,password,dataBase,sqlStr);
